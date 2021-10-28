c9a639ffd53050fe574a4abded98dbea06ae7ce4
Mateusz Wleklinski <wleklinskimateusz@gmail.com>
2021-10-28 23:29:17 +0200

    1 exercise

--- 03CustomTypes/01structures.rs
+++ 03CustomTypes/01structures.rs
@@ -20,5 +20,36 @@ struct Rectangle {
 }
 
 fn main() {
-    let name = String::from
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
+
+    println!("{:?}", peter);
+
+    let point = Point { x: 10.3, y: 0.4 };
+
+    println!("Point coordinates: ({}, {})", point.x, point.y);
+    
+    let bottom_right = Point { x: 5.2, ..point };
+
+    println!("Second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    let Point { x: left_edge, y: top_edge } = point;
+
+    let _rectangle = Rectangle {
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: bottom_right,
+    };
+
+    let _unit = Unit;
+
+    let pair = Pair(1, 0.1);
+
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+
+
 }
