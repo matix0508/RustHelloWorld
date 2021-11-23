70da3daa5e4bfc86a220c4cdef3bff4bcc2e4de6
Mateusz Wlekliński <matix0508@gmail.com>
2021-11-23 22:37:52 +0100

    Traits

---
+++ 16Traits/04Drop.rs
@@ -0,0 +1,34 @@
+struct Droppable {
+    name: &'static str,
+}
+
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("> Dropping {}", self.name);
+    }
+}
+
+fn main() {
+    let _a = Droppable { name: "a" };
+
+    {
+        let _b = Droppable {name: "b"};
+
+        {
+            let _c = Droppable { name: "c" };
+            let _d = Droppable { name: "d" };
+
+            println!("Exiting block B");
+        }
+        println!("just exited block B");
+
+        println!("Exiting block A");
+    }
+    println!("Just exitied block A");
+
+    drop(_a);
+
+    println!(
+        "end of the main function"
+    );
+}
